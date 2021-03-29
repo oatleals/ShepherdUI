6bb17712b198ba814a493fdf8413006b76086064
Leala Othman <leala.othman@slu.edu>
2021-03-28 19:48:33 -0500

    updated ClockIn UI

--- shepherd/lib/UI/ClockInForm.dart
+++ shepherd/lib/UI/ClockInForm.dart
@@ -1,104 +1,116 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shepherd/controllers/ClockController.dart';
 import 'package:shepherd/provider/GlobalState.dart';
 
-class data {
-  //test
-  int clientID = 0000;
-  String password = "";
-}
 
-class ClockInForm extends StatefulWidget {
+class ClockInForm extends StatefulWidget 
+{
   ClockInForm({Key key}) : super(key: key);
 
   @override
   _ClockInFormState createState() => _ClockInFormState();
 }
-
-class _ClockInFormState extends State<ClockInForm> {
+class _ClockInFormState extends State<ClockInForm> 
+{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) 
+  {
     // Grab reference of clientIdController from GlobalState so that we can
-    // connect it to the TextField where the user enters the Client ID.  This
-    // way, we can can reference the controller outside of this scope and get
+    // connect it to the TextField where the user enters the Client ID.  This 
+    // way, we can can reference the controller outside of this scope and get 
     // the Client ID value anywhere in the project.
-    GlobalState globalState = Provider.of<GlobalState>(context, listen: false);
-    TextEditingController textFieldController = globalState.clientIDController;
+    GlobalState globalState = Provider.of<GlobalState>(context);
+    TextEditingController clientIdText = globalState.clientIDController;
 
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(top: 8.0),
-          child: TextField(
-            style: TextStyle(fontSize: 35, color: Colors.blue),
-            controller: textFieldController,
-            keyboardType: TextInputType.number,
-            obscureText: false,
-            decoration: InputDecoration(
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.white,
-                  ),
+        Stack(
+          alignment: AlignmentDirectional.topEnd,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 0.0),
+              child: TextField(
+                style: TextStyle(
+                  fontSize:35,
+                  color: Colors.blue
                 ),
-                labelText: 'Client ID'),
-          ),
+                controller: clientIdText,
+                keyboardType: TextInputType.number,
+                obscureText: false,
+                decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      width: 2
+                    ),
+                  ),              
+                  labelText: 'Client ID'
+                ),
+              ),
+            ),
+            IconButton(
+              iconSize: 30,
+              color: Colors.white,
+              onPressed:() { Navigator.of(context).pop(); }, 
+              icon: Icon(Icons.close)
+            ),
+          ],
         ),
         Padding(
-          padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+          padding: const EdgeInsets.only(top:8.0, bottom:8.0),
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
-                width: 215,
+                width: 175,
                 child: TextField(
-                  style: TextStyle(fontSize: 35, color: Colors.blue),
+                  style: TextStyle(
+                    fontSize:35,
+                    color: Colors.blue
+                  ),
+                  controller: globalState.clockInPassController,
                   keyboardType: TextInputType.number,
                   obscureText: false,
                   decoration: InputDecoration(
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.white,
-                        ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 2
                       ),
-                      labelText: 'Password'),
+                    ),
+                    labelText: 'Password'
+                  ),
                 ),
               ),
               IconButton(
-                icon: Icon(Icons.camera_alt, size: 35, color: Colors.blue),
-                onPressed: () {}, // For scanning password.
+                icon: Icon(Icons.camera_alt, size: 30, color: Colors.blue),
+                onPressed: (){}, // For scanning password.
               )
             ],
           ),
         ),
         ElevatedButton(
-            onPressed: () {
-              final snackBar = SnackBar(
-                  content: Row(
-                children: [
-                  Text('Clock In: ',
-                      style: TextStyle(color: Colors.white, fontSize: 24)),
-                  Text('SUCCESS',
-                      style: TextStyle(color: Colors.green, fontSize: 24)),
-                ],
-              ));
+          onPressed: () async
+          { 
+            ClockController.clockIn(context);
+          },  
 
-              globalState.clockIn(clientId: textFieldController.text);
-
-              ScaffoldMessenger.of(context).showSnackBar(snackBar);
-              Navigator.of(context).pop();
-            },
-            child: Container(
-                // This is how to get the maximum width of the display.
-                width: MediaQuery.of(context).size.width - 150,
-                child: Center(
-                    child: Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("Clock In", style: TextStyle(fontSize: 40)),
-                  ),
-                )))),
+          child: Container(
+            // This is how to get the maximum width of the display.
+            width: MediaQuery.of(context).size.width - 150,
+            child: Center(
+              child: Center(child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("Clock In", style: TextStyle(fontSize: 40)),
+              ),)
+            )
+          )
+        ),
       ],
     );
   }
+
 }
