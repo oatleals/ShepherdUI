4767b6c79fc0b5b08e8eb94192afd784d21a6f05
Mike Donahoe <mike.donahoe@slu.edu>
2021-05-09 23:49:47 -0500

    changes to login page ui

--- shepherd/lib/UI/LoginPage.dart
+++ shepherd/lib/UI/LoginPage.dart
@@ -18,76 +18,76 @@ class _LoginPageState extends State<LoginPage> {
     
     return Scaffold(
       backgroundColor: Colors.blue[200], //Colors.blue[200]
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 40.0),
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                SizedBox(height: 10), //this moves the logo up or down
-                Image.asset('assets/logo.png'),
-                SizedBox(height: 40),
-                Text(
-                  'Sheppard Login',
-                  style: TextStyle(
-                      fontSize: 35,
-                      color: Colors.blue,
-                      fontWeight: FontWeight.bold),
-                )
-              ],
-            ),
-            SizedBox(height: 60.0),
-            TextField(
-              controller: employeeIDTextEditingController,
-              decoration: InputDecoration(
-                  labelText: "User ID",
-                  labelStyle: TextStyle(fontSize: 20),
-                  filled: true),
-            ),
-            TextField(
-              controller: emailTextEditingController,
-              decoration: InputDecoration(
-                  labelText: "Email Address",
-                  labelStyle: TextStyle(fontSize: 20),
-                  filled: true),
-            ),
-            SizedBox(height: 60.0),
-            SizedBox(height: 20),
-            Column(
-              children: <Widget>[
-                ButtonTheme(
-                  height: 50,
-                  disabledColor: Colors.blueAccent,
-                  child: ElevatedButton(
-                    onPressed: () async
-                    {
-                      final prefs = await SharedPreferences.getInstance();
-                      final email = emailTextEditingController.text;
-                      var employeeId;
-                      try {
-                        employeeId = int.parse(employeeIDTextEditingController.text);
-                      }
-                      catch(_) {
-                        employeeId = -1;
-                      }
-                      prefs.setInt('userId', employeeId);
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Image.asset('assets/logo.png', scale: 1.5,),
+                  Text(
+                    'Sheppard Login',
+                    style: TextStyle(
+                        fontSize: 35,
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              TextField(
+                controller: employeeIDTextEditingController,
+                decoration: InputDecoration(
+                    labelText: "User ID",
+                    labelStyle: TextStyle(fontSize: 20),
+                    filled: true),
+              ),
+              TextField(
+                controller: emailTextEditingController,
+                decoration: InputDecoration(
+                    labelText: "Email Address",
+                    labelStyle: TextStyle(fontSize: 20),
+                    filled: true),
+              ),
+              Column(
+                children: <Widget>[
+                  ButtonTheme(
+                    height: 50,
+                    disabledColor: Colors.blueAccent,
+                    child: ElevatedButton(
+                      onPressed: () async
+                      {
+                        final prefs = await SharedPreferences.getInstance();
+                        final email = emailTextEditingController.text;
+                        var employeeId;
+                        try {
+                          employeeId = int.parse(employeeIDTextEditingController.text);
+                        }
+                        catch(_) {
+                          employeeId = -1;
+                        }
+                        prefs.setInt('userId', employeeId);
 
 
-                      var status = await requestEmailConfirmation(email, employeeId);                      
-                      if (status == ERROR.success)
-                        Navigator.of(context).pushReplacementNamed("/LoginOTP");
-                      else 
-                        showSnackbar(context, status);
-                    },
-                    child: Text(
-                      'Request Email Verification',
-                      style: TextStyle(fontSize: 20, color: Colors.white),
+                        var status = await requestEmailConfirmation(email, employeeId);                      
+                        if (status == ERROR.success)
+                          Navigator.of(context).pushReplacementNamed("/LoginOTP");
+                        else 
+                          showSnackbar(context, status);
+                      },
+                      child: Text(
+                        'Request Email Verification',
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
                     ),
-                  ),
-                )
-              ],
-            )
-          ],
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
